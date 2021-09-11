ca5db785b59bd31a50bcad72be6b279e26c674cc
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-11 14:40:07 +0900

    バリデーション設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true
-  validatea :last_name, presence: true
+  validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_furigane, presence: true
+  validates :last_name_furigana, presence: true
   validates :first_name_furigana, presence: true
   validates :birthday, presence: true
 end
