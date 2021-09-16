34fff7ff12a036ff70a15230d7f52fcd73ed5668
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-16 21:58:16 +0900

    バリデーションの設定4

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,17 @@ class User < ApplicationRecord
   validates :last_name_furigana, presence: true
   validates :first_name_furigana, presence: true
   validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_furigana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_furigana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is including both English letter and number' 
+
+  has_many :items
+  has_many :purchases
 end
