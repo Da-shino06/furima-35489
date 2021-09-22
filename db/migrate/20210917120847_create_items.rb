0bddc47ef2fdfa1ffd8eda8d6a2a3904d76e8174
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-22 18:33:40 +0900

    マイグレーション設定

--- db/migrate/20210917120847_create_items.rb
+++ db/migrate/20210917120847_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :product_name, null: false
+      t.string     :product_name,        null: false
       t.text       :description_of_item, null: false
       t.integer    :product_category_id, null: false
-      t.integer    :product_status_id, null: false
-      t.integer    :shipping_charge_id, null: false
-      t.integer    :shipping_area_id, null: false
-      t.integer    :day_to_ship_id, null: false
-      t.integer    :selling_price, null: false
-      t.references :user, null: false, foreign_key: true
+      t.integer    :product_status_id,   null: false
+      t.integer    :shipping_charge_id,  null: false
+      t.integer    :shipping_area_id,    null: false
+      t.integer    :day_to_ship_id,      null: false
+      t.integer    :selling_price,       null: false
+      t.references :user,                null: false, foreign_key: true
       t.timestamps
     end
   end
