cccc5a18c149d56efb32d4b572619758325bbd6c
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-23 08:59:46 +0900

    修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product_name, presence: true
   validates :description_of_item, presence: true
-  validates :selling_price, presence: true, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  validates :selling_price, presence: true,
+                            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :product_category
@@ -11,7 +12,8 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :day_to_ship
 
-  validates :product_category_id, :product_status_id, :shipping_charge_id, :shipping_area_id, :day_to_ship_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :product_category_id, :product_status_id, :shipping_charge_id, :shipping_area_id, :day_to_ship_id,
+            numericality: { other_than: 0, message: "can't be blank" }
 
   belongs_to :user
   has_one_attached :image
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    context "商品が出品できる場合" do
-      it "商品画像、商品名、商品の説明、商品カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格があれば商品は出品できる" do
+    context '商品が出品できる場合' do
+      it '商品画像、商品名、商品の説明、商品カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格があれば商品は出品できる' do
         expect(@item).to be_valid
       end
     end
-    context "商品が出品できない場合" do
+    context '商品が出品できない場合' do
       it '商品画像が空では出品できない' do
         @item.image = nil
         @item.valid?
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description of item can't be blank")
       end
       it '商品カテゴリーが空では出品できない' do
-        @item.product_category_id = '--'
+        @item.product_category_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Product category can't be blank")
       end
       it '商品状態が空では出品できない' do
-        @item.product_status_id= '--'
+        @item.product_status_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Product status can't be blank")
       end
       it '配送料の負担が空では出品できない' do
-        @item.shipping_charge_id = '--'
+        @item.shipping_charge_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
       end
       it '発送元の地域が空では出品できない' do
-        @item.shipping_area_id = '--'
+        @item.shipping_area_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area can't be blank")
       end
       it '発送までの日数が空では出品できない' do
-        @item.day_to_ship_id = '--'
+        @item.day_to_ship_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Day to ship can't be blank")
       end
@@ -60,12 +60,22 @@ RSpec.describe Item, type: :model do
       it '販売価格が¥300~¥9,999,999の間でないと出品できない' do
         @item.selling_price = '100'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Selling price must be greater than or equal to 300')
       end
       it '販売価格は半角数字で記入しないと出品できない' do
         @item.selling_price = 'AAA'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include('Selling price is not a number')
+      end
+      it '販売価格が¥10,000,000以上では出品できない' do
+        @item.selling_price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Selling price must be less than or equal to 9999999')
+      end
+      it 'Userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
