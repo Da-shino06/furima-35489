af07f50a8e60ede0b7ac81bf64aad34ac23e953f
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-27 19:10:21 +0900

    商品購入テストコードの設定

---
+++ spec/factories/purchase_shipping_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchase_shipping_address do
+    postel_code      { '123-4567' }
+    shipping_area_id { 1 }
+    municipality     { '横浜市' }
+    address          { '青山1-1-1' }
+    building_name    { '柳ビル103' }
+    telephone_number { "19012345678" }
+
+    association :user
+    association :item
+  end
+end
