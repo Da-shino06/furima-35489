893dcc5fded3c7db9d88f0cbd4a8e6e9f6551f1c
Da-shino06 <83986143+Da-shino06@users.noreply.github.com>
2021-09-29 00:01:56 +0900

    rubocop導入2

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,7 +1,9 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postel_code, :shipping_area_id, :municipality, :address, :building_name, :telephone_number
+  attr_accessor :token, :user_id, :item_id, :postel_code, :shipping_area_id, :municipality, :address, :building_name,
+                :telephone_number
 
+  validates :token, presence: true
   validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :municipality, presence: true
   validates :address, presence: true
@@ -9,12 +11,13 @@ class PurchaseShippingAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postel_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :telephone_number, presence: true, format: {with: /\A\d{11}\z/, message: "is invalid"}
+    validates :postel_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :telephone_number, presence: true, format: { with: /\A\d{11}\z/, message: 'is invalid' }
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postel_code: postel_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+    ShippingAddress.create(postel_code: postel_code, shipping_area_id: shipping_area_id, municipality: municipality,
+                           address: address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
